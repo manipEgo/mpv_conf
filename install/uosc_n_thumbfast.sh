ba216ba0aca93fd0709dc2dad14b8a9318f41f2b
manip_ego <52987267+manipEgo@users.noreply.github.com>
2023-03-26 01:56:57 +0800

    install folder

---
+++ install/uosc_n_thumbfast.sh
@@ -0,0 +1,7 @@
+workdir=$(cd $(dirname $0); cd ..; pwd)
+mkdir -pv ${workdir}/script-opts/
+rm -rf ${workdir}/scripts/uosc_shared
+wget -P /tmp/ https://github.com/tomasklaen/uosc/releases/latest/download/uosc.zip
+unzip -od ${workdir}/ /tmp/uosc.zip
+rm -fv /tmp/uosc.zip
+wget -NP ${workdir}/scripts/ https://raw.githubusercontent.com/po5/thumbfast/master/thumbfast.lua
