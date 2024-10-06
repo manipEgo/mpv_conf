a817f5fed6f7f838335361410822ce8e3360705a
manip_ego <52987267+manipEgo@users.noreply.github.com>
2024-10-06 14:00:04 +0800

    update: separately download uosc config

--- install/uosc_n_thumbfast.sh
+++ install/uosc_n_thumbfast.sh
@@ -1,5 +1,6 @@
 workdir=$(cd $(dirname $0); cd ..; pwd)
 mkdir -pv ${workdir}/script-opts/
+wget -NP ${workdir}/script-opts/ https://github.com/tomasklaen/uosc/releases/latest/download/uosc.conf
 rm -rf ${workdir}/scripts/uosc_shared
 wget -P /tmp/ https://github.com/tomasklaen/uosc/releases/latest/download/uosc.zip
 unzip -od ${workdir}/ /tmp/uosc.zip
